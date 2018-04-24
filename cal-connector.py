0c3feed2c289cc4c32b139a3a5948e2bba3b05af
Bryan Reed <bryan87reed@gmail.com>
2018-04-23 22:01:04 -0700

    Gathers input needed. Outlines Zimbra return body

---
+++ cal-connector.py
@@ -0,0 +1,32 @@
+import requests, json, datetime
+
+import redtail
+
+rt_username = raw_input('Redtail Username: ')
+rt_password = raw_input('Redtail Password: ')
+zimbra_username = raw_input('Zimbra Webmail Email Address: ')
+zimbra_password = raw_input('Zimbra Webmail Password: ')
+email_domain = raw_input('Email Domain: ')
+
+zimbra_url = 'http://webmail.{}/home/{}'.format(email_domain, zimbra_username)
+zimbra_basic_auth = '&auth=ba'
+
+zimbra_cal = zimbra_url + 'calendar?fmt=json&start=-1mon&end=+1year'
+
+def get_cal():
+    r = requests.get(zimbra_cal + zimbra_basic_auth, auth=(zimbra_username, zimbra_password))
+    # Has to return the whole calendar
+    # 'appt: ['inv': [
+    #                   {'type': 'appt',
+    #                    'comp': [
+    #                           {
+    #                            'name': 'cal_item_name',
+    #                            'fr': 'short description'
+    #                            's': [{'d':timestamp
+    #                                   'tz':timezone
+    #                                  }]                              
+    #                            }
+    #                             ]
+    #                       }
+    #               ]
+    #           ]
